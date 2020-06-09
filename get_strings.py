f5e996945b0f3875fa35b0ef7b09402306501b03
aambrioso <aambrioso1@gmail.com>
2020-06-09 04:58:54 -0400

    Took out a line of code.

--- get_strings.py
+++ get_strings.py
@@ -1,8 +1,7 @@
 from collections import Counter
 
 def get_strings(city):
-	city = city.lower()
-	letters = Counter(city)
+	letters = Counter(city.lower())
 	list1 = []
 	for item in letters:
 		if item.isalpha():
