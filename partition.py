f54bb9f273b269a103c38b06df1ee7f99bdb02f1
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2021-05-24 14:20:09 -0400

    adding a couple of programs denominator.py and a shell for partition.py

---
+++ partition.py
@@ -0,0 +1,27 @@
+"""
+Explosive Sum
+https://www.codewars.com/kata/52ec24228a515e620b0005ef
+
+https://en.wikipedia.org/wiki/Partition_(number_theory)#
+
+This a classic problem.   I will try to implement it in different ways.
+Not too concerned about working it out from scratch.
+
+
+
+**** Sample Tests ****
+test.describe('testing exp_sum')
+test.it('***** Very basic tests *****\n')
+test.assert_equals(exp_sum(1), 1)
+test.assert_equals(exp_sum(2), 2)
+test.assert_equals(exp_sum(3), 3)
+test.it('_____ So far so good _____\n')
+test.it('\n***** Funcionality tests *****\n')
+test.assert_equals(exp_sum(4), 5)
+test.assert_equals(exp_sum(5), 7)
+test.assert_equals(exp_sum(10), 42)
+"""
+
+def exp_sum(n):
+    #your code here
+
